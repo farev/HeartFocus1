bca46efbd898e4fb299dd43bef6b2086cb2292a9
Fabian <fabiareor@gmail.com>
2024-09-14 16:35:44 -0400

    Initial Commit

---
+++ HeartFocus2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HeartFocus2
+//
+//  Created by Fabian  Arevalo on 9/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HeartFocus2/HeartFocus2App.swift
@@ -0,0 +1,17 @@
+//
+//  HeartFocus2App.swift
+//  HeartFocus2
+//
+//  Created by Fabian  Arevalo on 9/14/24.
+//
+
+import SwiftUI
+
+@main
+struct HeartFocus2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
